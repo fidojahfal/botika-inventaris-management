0d12e1fb84a0a5f1351a5c9a0994e5b670ca15c0
fidojahfal <fidojahfalprayoga@gmail.com>
2026-02-02 23:08:08 +0700

    feat: add update user api for updating user

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -46,27 +46,33 @@ class UserController extends Controller
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'name' => 'string|required',
+                'jabatan' => 'string|required',
+                'department_id' => 'integer|required|exists:departments,id',
+            ]
+        );
+
+        if ($validate->fails()) {
+            return response()->json([
+                "message" => "Failed to input user, please check your input!",
+                "errors" => $validate->errors(),
+            ], 422);
+        }
+
+        $validatedData = $validate->validated();
+        $user->update($validatedData);
+
+        return response()->json([
+            "message" => 'Success',
+            "data" => $user,
+        ], 201);
     }
 
     /**
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,4 +21,5 @@ Route::post("/login", [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::post('/users/create', [UserController::class, 'store']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
 });
