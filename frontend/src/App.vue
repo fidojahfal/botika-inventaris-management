20d698c32d82b9df61f50539c2ea4cd33802615b
fidojahfal <fidojahfalprayoga@gmail.com>
2026-02-03 04:03:17 +0700

    feat: add router view to handle routing in vue and use it in main

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <div style="bg-red-100">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-100">
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-
 .logo {
   height: 6em;
   padding: 1.5em;
