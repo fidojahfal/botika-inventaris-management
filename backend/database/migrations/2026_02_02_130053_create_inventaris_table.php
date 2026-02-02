a46d1930af4a37311bf9bada0e65071977b81772
fidojahfal <fidojahfalprayoga@gmail.com>
2026-02-02 20:17:10 +0700

    feat: create migration and model for inventaris table

---
+++ backend/database/migrations/2026_02_02_130053_create_inventaris_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventarisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventaris', function (Blueprint $table) {
+            $table->string("id")->primary();
+            $table->string("name");
+            $table->foreignId("type_id")->nullable()->constrained("type_inventaris")->nullOnDelete();
+            $table->string("serial_number")->unique();
+            $table->string("spesifikasi");
+            $table->foreignId("status_id")->constrained("status_inventaris");
+            $table->foreignId("assigned_user_id")->nullable()->constrained("users")->nullOnDelete();
+            $table->foreignId("department_id")->nullable()->constrained("departments")->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventaris');
+    }
+}
