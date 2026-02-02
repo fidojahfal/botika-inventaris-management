c4e70dfafa008167d3c51a1013004d6340c385a0
fidojahfal <fidojahfalprayoga@gmail.com>
2026-02-02 20:48:21 +0700

    feat: add inventaris relation and model fillable

--- backend/app/Models/Inventaris.php
+++ backend/app/Models/Inventaris.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Inventaris extends Model
 {
     use HasFactory;
+
+    protected $table = "inventaris";
+    protected $fillable = ["name", "type_id", "serial_number", "spesifikasi", "status_id", "assigned_user_id", "department_id"];
+
+    public function type()
+    {
+        return $this->belongsTo(type_inventaris::class, "type_id");
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status_inventaris::class, "status_id");
+    }
+
+    public function assignedUser()
+    {
+        return $this->belongsTo(User::class, "assigned_user_id");
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
