93d454ab9043ccf0cd2ba013dcbfb96aa5524582
fidojahfal <fidojahfalprayoga@gmail.com>
2026-02-02 22:09:44 +0700

    feat: change response of failed login and add failed validate

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -5,18 +5,26 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+        $validator = Validator::make(
+            $request->only(['email', 'password']),
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Failed to validate user login!'], 401);
+        }
 
         if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Email or password is wrong!'], 401);
         }
 
         $token = $request->user()->createToken('api-token')->plainTextToken;
