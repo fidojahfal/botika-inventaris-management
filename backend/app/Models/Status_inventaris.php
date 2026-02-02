1654bdd17d7025b2355db6d6299eeb5e22805945
fidojahfal <fidojahfalprayoga@gmail.com>
2026-02-02 20:45:49 +0700

    feat: add status inventaris model relation

--- backend/app/Models/Status_inventaris.php
+++ backend/app/Models/Status_inventaris.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Status_inventaris extends Model
 {
     use HasFactory;
+
+    protected $table = "status_inventaris";
+    protected $fillable = ["name"];
+
+    public function inventaris()
+    {
+        return $this->hasMany(Inventaris::class, "status_id");
+    }
 }
