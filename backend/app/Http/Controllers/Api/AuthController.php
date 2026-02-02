8d44fd8ccf1ff232cf96cb93de39ff251ec671d5
fidojahfal <fidojahfalprayoga@gmail.com>
2026-02-02 22:08:28 +0700

    feat: add login api for user login before do anything in api

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $token = $request->user()->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $request->user(),
+        ]);
+    }
+}
