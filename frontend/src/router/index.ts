05467bf8a3d7e90c7c8d62d21bf7d6a4b26aca0c
fidojahfal <fidojahfalprayoga@gmail.com>
2026-02-03 05:48:40 +0700

    fix: bug can't protect every routes even login or no login

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../pages/Login.vue";
+import Users from "../pages/Users.vue";
 
-const routes = [{ path: "/login", component: Login }];
+const routes = [
+  { path: "/login", component: Login, meta: { guest: true } },
+  { path: "/users", component: Users, meta: { requiresAuth: true } },
+];
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,6 +17,8 @@ router.beforeEach((to, _, next) => {
 
   if (to.meta.requiresAuth && !token) {
     next("/login");
+  } else if (to.meta.guest && token) {
+    next("/users");
   } else {
     next();
   }
